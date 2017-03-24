dcbb1cf1e8c55cbf1370d246a97b5cb629283e5b
João Mendes <Joo Mendes@172.30.19.182>
2017-03-24 15:27:44 +0000

    ogre unit testing

--- dkeep/src/dkeep/test/TestGuardRookieGameLogic.java
+++ dkeep/src/dkeep/test/TestGuardRookieGameLogic.java
@@ -1,40 +1,29 @@
 package dkeep.test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import dkeep.logic.CellPosition;
 import dkeep.logic.Direction;
 import dkeep.logic.Game;
 import dkeep.logic.GameMap;
+import dkeep.logic.Personality;
 import dkeep.logic.State;
 
-public class TestKeepGameLogic {
 
-		char [][] map = new char [][] 
-				{{'X','X','X','X','X'},
-				{'X','H',' ','O','X'},
-				{'I',' ',' ',' ','X'},
-				{'X','k',' ',' ','X'},
-				{'X','X','X','X','X'}
-	};
+public class TestGuardRookieGameLogic {
 	
 	@Test
 	public void testMoveHeroIntoFreeCell() {
-		GameMap gameMap = new GameMap(map);
-		Game game = new Game(gameMap);
+		Game game = new Game(Personality.ROOKIE);
 		assertEquals(new CellPosition(1,1), game.getHeroPosition());
 		game.updateGame(Direction.DOWN);
 		assertEquals(new CellPosition(1,2), game.getHeroPosition());
 	}
-	
+
 	@Test
 	public void testMoveHeroIntoWall() {
-		GameMap gameMap = new GameMap(map);
-		Game game = new Game(gameMap);
+		Game game = new Game(Personality.ROOKIE);
 		assertEquals(new CellPosition(1,1), game.getHeroPosition());
 		game.updateGame(Direction.UP);
 		assertEquals(new CellPosition(1,1), game.getHeroPosition());
@@ -42,8 +31,7 @@ public class TestKeepGameLogic {
 	
 	@Test
 	public void testHeroIsCapturedByGuard() {
-		GameMap gameMap = new GameMap(map);
-		Game game = new Game(gameMap);
+		Game game = new Game(Personality.ROOKIE);
 		assertFalse(game.isGameOver());
 		game.updateGame(Direction.RIGHT);
 		assertTrue(game.isGameOver());
@@ -51,9 +39,20 @@ public class TestKeepGameLogic {
 	}	
 	
 	@Test
+	public void testHeroMoveDiagonal() {
+		Game game = new Game(Personality.ROOKIE);
+		assertFalse(game.isGameOver());
+		game.updateGame(Direction.DOWN);
+		assertFalse(game.isGameOver());
+		game.updateGame(Direction.RIGHT);
+		assertFalse(game.isGameOver());		
+		assertEquals(State.LEVEL1, game.getState());
+		assertEquals(new CellPosition(2,2), game.getHeroPosition());
+	}	
+	
+	@Test
 	public void testMoveHeroIntoClosedDoor() {
-		GameMap gameMap = new GameMap(map);
-		Game game = new Game(gameMap);
+		Game game = new Game(Personality.ROOKIE);
 		assertEquals(new CellPosition(1,1), game.getHeroPosition());
 		game.updateGame(Direction.DOWN);
 		assertEquals(new CellPosition(1,2), game.getHeroPosition());
@@ -63,8 +62,7 @@ public class TestKeepGameLogic {
 	
 	@Test
 	public void testMoveHeroIntoLever() {
-		GameMap gameMap = new GameMap(map);
-		Game game = new Game(gameMap);
+		Game game = new Game(Personality.ROOKIE);
 		assertEquals(new CellPosition(1,1), game.getHeroPosition());
 		game.updateGame(Direction.DOWN);
 		assertEquals(new CellPosition(1,2), game.getHeroPosition());
@@ -76,8 +74,7 @@ public class TestKeepGameLogic {
 	
 	@Test
 	public void testMoveHeroIntoNextLevel() {
-		GameMap gameMap = new GameMap(map);
-		Game game = new Game(gameMap);
+		Game game = new Game(Personality.ROOKIE);
 		assertEquals(new CellPosition(1,1), game.getHeroPosition());
 		game.updateGame(Direction.DOWN);
 		assertEquals(new CellPosition(1,2), game.getHeroPosition());
@@ -90,6 +87,5 @@ public class TestKeepGameLogic {
 		assertFalse(game.isGameOver());
 		assertEquals(State.LEVEL2, game.getState());
 	}
-
-
+	
 }
---
+++ dkeep/src/dkeep/test/TestStillOgreGameLogic.java
@@ -0,0 +1,95 @@
+package dkeep.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import dkeep.logic.CellPosition;
+import dkeep.logic.Direction;
+import dkeep.logic.Game;
+import dkeep.logic.GameMap;
+import dkeep.logic.State;
+
+public class TestStillOgreGameLogic {
+
+		char [][] map = new char [][] 
+				{{'X','X','X','X','X'},
+				{'X','H',' ','O','X'},
+				{'I',' ',' ',' ','X'},
+				{'X','k',' ',' ','X'},
+				{'X','X','X','X','X'}
+	};
+	
+	@Test
+	public void testMoveHeroIntoFreeCell() {
+		GameMap gameMap = new GameMap(map);
+		Game game = new Game(gameMap);
+		assertEquals(new CellPosition(1,1), game.getHeroPosition());
+		game.updateGame(Direction.DOWN);
+		assertEquals(new CellPosition(1,2), game.getHeroPosition());
+	}
+	
+	@Test
+	public void testMoveHeroIntoWall() {
+		GameMap gameMap = new GameMap(map);
+		Game game = new Game(gameMap);
+		assertEquals(new CellPosition(1,1), game.getHeroPosition());
+		game.updateGame(Direction.UP);
+		assertEquals(new CellPosition(1,1), game.getHeroPosition());
+	}
+	
+	@Test
+	public void testHeroIsCapturedByGuard() {
+		GameMap gameMap = new GameMap(map);
+		Game game = new Game(gameMap);
+		assertFalse(game.isGameOver());
+		game.updateGame(Direction.RIGHT);
+		assertTrue(game.isGameOver());
+		assertEquals(State.DEFEAT, game.getState());
+	}	
+	
+	@Test
+	public void testMoveHeroIntoClosedDoor() {
+		GameMap gameMap = new GameMap(map);
+		Game game = new Game(gameMap);
+		assertEquals(new CellPosition(1,1), game.getHeroPosition());
+		game.updateGame(Direction.DOWN);
+		assertEquals(new CellPosition(1,2), game.getHeroPosition());
+		game.updateGame(Direction.LEFT);
+		assertEquals(new CellPosition(1,2), game.getHeroPosition());
+	}
+	
+	@Test
+	public void testMoveHeroIntoLever() {
+		GameMap gameMap = new GameMap(map);
+		Game game = new Game(gameMap);
+		assertEquals(new CellPosition(1,1), game.getHeroPosition());
+		game.updateGame(Direction.DOWN);
+		assertEquals(new CellPosition(1,2), game.getHeroPosition());
+		game.updateGame(Direction.DOWN);
+		assertEquals(new CellPosition(1,3), game.getHeroPosition());
+		assertEquals('S', map[2][0]);
+		assertEquals('S', map[3][0]);
+	}
+	
+	@Test
+	public void testMoveHeroIntoNextLevel() {
+		GameMap gameMap = new GameMap(map);
+		Game game = new Game(gameMap);
+		assertEquals(new CellPosition(1,1), game.getHeroPosition());
+		game.updateGame(Direction.DOWN);
+		assertEquals(new CellPosition(1,2), game.getHeroPosition());
+		game.updateGame(Direction.DOWN);
+		assertEquals(new CellPosition(1,3), game.getHeroPosition());
+		assertEquals('S', map[2][0]);
+		assertEquals('S', map[3][0]);
+		game.updateGame(Direction.LEFT);
+		assertEquals(new CellPosition(1,7), game.getHeroPosition()); //keep level position
+		assertFalse(game.isGameOver());
+		assertEquals(State.LEVEL2, game.getState());
+	}
+
+
+}
