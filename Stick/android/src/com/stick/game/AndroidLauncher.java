2396d2f2b0c70aeb53367d91d34453eba013c248
João Mendes <up201505439@fe.up.pt>
2017-05-22 19:39:06 +0100

    Add files via upload

---
+++ Stick/android/src/com/stick/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.stick.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.stick.game.StickGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StickGame(), config);
+	}
+}
