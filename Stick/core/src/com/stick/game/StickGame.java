cc6cf26434e631a4fe7e1308ad6ec875396db877
JMendes25 <up201505439@fe.up.pt>
2017-06-11 23:19:00 +0100

    least

--- Stick/core/src/com/stick/game/StickGame.java
+++ Stick/core/src/com/stick/game/StickGame.java
@@ -2,32 +2,56 @@ package com.stick.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.stick.game.states.Menu;
+import com.stick.game.states.StateManager;
 
 public class StickGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+    public static SpriteBatch batch;
+    public static Preferences prefs;
+    private StateManager manager;
+
+    public static final int V_WIDTH = 250;
+    public static final int V_HEIGHT = 400;
+    public static final float PPM = 4f;
+
+    public static Music song;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
-
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 34/255f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+        prefs = Gdx.app.getPreferences("score");
+        if(!prefs.contains("highscore"))
+            prefs.putInteger("highscore", 0);
+        manager = new StateManager();
+        manager.push(new Menu(manager));
+        song = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        song.setLooping(true); song.setVolume(0.2f); song.play();
 	}
 	
 	@Override
 	public void dispose () {
+        super.dispose();
 		batch.dispose();
-		img.dispose();
+        song.dispose();
+        prefs.flush();
 	}
+
+	@Override
+    public void render(){
+        super.render();
+        Gdx.gl.glClearColor(15/255f,120/255f,230/255f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        manager.update(Gdx.graphics.getDeltaTime());
+        manager.render(batch);
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
+    }
 }
--- Stick/desktop/src/com/stick/game/desktop/DesktopLauncher.java
+++ Stick/desktop/src/com/stick/game/desktop/DesktopLauncher.java
@@ -9,12 +9,12 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Stick";
-		config.width = 480;
-		config.height = 320;
-		config.resizable = false;
+		config.width = StickGame.V_WIDTH;
+		config.height = StickGame.V_HEIGHT;
+		/*config.resizable = false;
 		config.useGL30 = false;
 		//config.addIcon("C:\\LPOO\\Stick\\android\\res\\drawable-mhdpi\\ic_launcher.png", Files.FileType.Internal);
-
+		*/
 		new LwjglApplication(new StickGame(), config);
 	}
 }
